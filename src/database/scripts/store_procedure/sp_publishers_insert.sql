e96a61ae5f803e0d9a5cad0dd825ffe9a06f2ae7
Renato Soares <renatosoares@mobister.com.br>
2018-08-22 09:17:44 -0300

    store procedure insert

---
+++ src/database/scripts/store_procedure/sp_publishers_insert.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION public.sp_publishers_insert(
+    pub_name varchar(4),
+    pub_city varchar(20),
+    pub_state bpchar(2),
+    pub_country varchar(30)
+) RETURNS void LANGUAGE sql
+AS
+$function$
+    INSERT INTO public.publishers (
+        name, city, state, country
+    ) VALUES (
+        pub_name, pub_city, pub_state, pub_country
+    )
+$function$
