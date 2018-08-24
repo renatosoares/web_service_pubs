8451497ea5abc2728ed523ad88390dfeabffbf46
Renato Soares <renatosoares@mobister.com.br>
2018-08-24 19:08:07 -0300

    WIP - delete and update store procedure

---
+++ src/database/scripts/store_procedure/sp_publishers_delete.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION public.sp_publishers_delete(
+    pub_id integer,
+) RETURNS void LANGUAGE sql
+AS
+$function$
+
+    DELETE FROM public.publishers
+    WHERE id = pub_id
+
+$function$
---
+++ src/database/scripts/store_procedure/sp_publishers_update.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION public.sp_publishers_update(
+    pub_id integer,
+    pub_name varchar(40),
+    pub_city varchar(20),
+    pub_state bpchar(2),
+    pub_country varchar(30)
+) RETURNS void LANGUAGE sql
+AS
+$function$
+    UPDATE public.publishers
+    SET "name"=pub_name, city=pub_city, state=pub_state, country='', created_at='', updated_at=''
+    WHERE id=pub_id;
+$function$
