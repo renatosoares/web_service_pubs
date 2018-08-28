be96b1a4786d015f7300196e8bf80f1a322a8a47
Renato Soares <renatosoares@mobister.com.br>
2018-08-28 09:02:20 -0300

    create resource

--- src/app/Http/Controllers/PublisherController.php
+++ src/app/Http/Controllers/PublisherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Publisher;
 use Illuminate\Http\Request;
 
 class PublisherController extends Controller
@@ -23,7 +24,7 @@ class PublisherController extends Controller
      */
     public function create()
     {
-        //
+        return view('publisher.create');
     }
 
     /**
@@ -40,10 +41,10 @@ class PublisherController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Publisher  $publisher
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Publisher $publisher)
     {
         //
     }
@@ -51,10 +52,10 @@ class PublisherController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Publisher  $publisher
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Publisher $publisher)
     {
         //
     }
@@ -63,10 +64,10 @@ class PublisherController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Publisher  $publisher
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Publisher $publisher)
     {
         //
     }
@@ -74,10 +75,10 @@ class PublisherController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Publisher  $publisher
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Publisher $publisher)
     {
         //
     }
---
+++ src/resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        @section('sidebar')
+            This is the master sidebar.
+        @show
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
