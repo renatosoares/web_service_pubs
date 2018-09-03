af43e61cac902461c6d233848e23ad4d4810f6c5
Renato Soares <renatosoarespro@hotmail.com>
2018-09-02 22:48:01 -0300

    add cors

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    'middleware' => ['api']
+], function ($router) {
+     Route::apiResource('/publishers','PublisherController');
 });
-
-Route::resource('publishers', 'PublisherController');
